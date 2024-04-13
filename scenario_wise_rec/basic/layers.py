071dc8a3ed4d80a8d5d68319a73ae03addd7c31d
XiaopengLi <xiaopli2-c@my.cityu.edu.hk>
2024-04-13 17:58:01 +0800

    GateNU upload

--- scenario_wise_rec/basic/layers.py
+++ scenario_wise_rec/basic/layers.py
@@ -303,3 +303,18 @@ class Pruner(nn.Module):
             vout = self.beta*self.sigmoid(vin*alpha)
             s = vout * torch.sign(vout - self.epsilon)
             return s
+
+class GateNU(nn.Module):
+    def __init__(self, input_dim, output_dim, hidden_dim=None, gemma=2.0):
+        super(GateNU, self).__init__()
+        if hidden_dim is None:
+            hidden_dim = output_dim
+        self.gemma = gemma
+        self.network = nn.Sequential(nn.Linear(input_dim, hidden_dim),
+                                    nn.ReLU(),
+                                    nn.Linear(hidden_dim, output_dim),
+                                    nn.Sigmoid())
+
+    def forward(self, inputs):
+        out_layer = self.network(inputs)
+        return out_layer * self.gemma
