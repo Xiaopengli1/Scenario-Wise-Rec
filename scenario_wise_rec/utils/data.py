649e20daf703b3d17dc051799b2c0e9231ddbe3d
Xiaopeng Li <41949371+Xiaopengli1@users.noreply.github.com>
2024-08-16 10:43:12 +0800

    Update data.py

--- scenario_wise_rec/utils/data.py
+++ scenario_wise_rec/utils/data.py
@@ -56,9 +56,9 @@ class DataGenerator(object):
             val_dataset = TorchDataset(x_val, y_val)
             test_dataset = TorchDataset(x_test, y_test)
 
-        train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-        val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-        test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+        train_dataloader = DataLoader(train_dataset, batch_size = batch_size, shuffle = True, num_workers = num_workers)
+        val_dataloader = DataLoader(val_dataset, batch_size = batch_size, shuffle = False, num_workers = num_workers)
+        test_dataloader = DataLoader(test_dataset, batch_size = batch_size, shuffle = False, num_workers = num_workers)
         return train_dataloader, val_dataloader, test_dataloader
 
 
